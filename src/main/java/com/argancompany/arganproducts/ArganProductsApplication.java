8f562a11551610b910670e07a420c99aed70c664
hbouksim <hafida.bouksim@sofrecom.com>
2023-08-28 22:38:47 +0100

    initiate project

---
+++ src/main/java/com/argancompany/arganproducts/ArganProductsApplication.java
@@ -0,0 +1,13 @@
+package com.argancompany.arganproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArganProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArganProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argancompany/arganproducts/ArganProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argancompany.arganproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArganProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
